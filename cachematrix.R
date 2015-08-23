2235c3a13c3f877478deea6bf2920b4e25ac6957
Auxence <auxence.gros-borot@laposte.net>
2015-08-23 13:59:46 +0200

    Commit with makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -2,17 +2,47 @@
 ## functions do
 
 ## Write a short comment describing this function
+# Heavily inspired on the makeVector example, the makeCacheMatrix
+#take as an argument a matrix, computes the inverse. Then it returns a list
+# with the different features of the matri and the computation.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(X = matrix()) {
+  
+      inv <- NULL # This variable will cache the invert
+  
+        set <- function(Y) {
+            X <<- Y
+            inv <<- NULL
+        }
+        
+        get <- function() X
+        setinv <- function(Inverse) inv <<- solve #Replace mean() with solve()
+        getinv <- function() inv
+        
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
+        
 
 }
 
 
 ## Write a short comment describing this function
+#Again this function is heavily inspired by the sample, with very
+#little differences except that m is replaced by inv.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+cacheSolve <- function(X, ...) {
+        ## Return a matrix that is the inverse of 'X'
+        inv <- X$getinv()
+        
+        if (!is.null(inv)) #Test if the inverse exists.          {
+          
+          message ("Getting cache data - inverse already previously computed")
+          
+          return (inv)
+        }
+        
+        data <- X$get()
+        inv <- solve(data, ...)
+        X$setinv(inv)
+        inv
 }
-
-
-##THIS IS A TEST FOR GITHUB UPLOAD
